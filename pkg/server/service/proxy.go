332f0f18f12806a10d68c51a521cbb812d8bb6d4
Julien Salleyron <julien.salleyron@gmail.com>
2022-09-21 15:58:28 +0200

    Test

--- pkg/server/service/proxy.go
+++ pkg/server/service/proxy.go
@@ -7,13 +7,13 @@ import (
 	"io"
 	"net"
 	"net/http"
-	"net/http/httputil"
 	"net/url"
 	"strings"
 	"time"
 
 	ptypes "github.com/traefik/paerser/types"
 	"github.com/traefik/traefik/v2/pkg/config/dynamic"
+	"github.com/traefik/traefik/v2/pkg/httputil"
 	"github.com/traefik/traefik/v2/pkg/log"
 	"github.com/valyala/fasthttp"
 )
@@ -37,61 +37,78 @@ var hopHeaders = []string{
 }
 
 func newFastHTTPReverseProxy(client *fasthttp.Client) (http.Handler, error) {
-	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-		req, res := fasthttp.AcquireRequest(), fasthttp.AcquireResponse()
-		for _, header := range hopHeaders {
-			request.Header.Del(header)
-		}
-		fasthttp.AcquireURI()
-		req.Header.Set("FastHTTP", "enable")
-		req.Header.SetHost(request.Host)
-		req.SetRequestURI(request.RequestURI)
-		req.SetHost(request.URL.Host)
 
-		for k, v := range request.Header {
-			req.Header.Set(k, strings.Join(v, ", "))
-		}
-
-		req.SetBodyStream(request.Body, int(request.ContentLength))
-
-		req.Header.SetMethod(request.Method)
-
-		if clientIP, _, err := net.SplitHostPort(request.RemoteAddr); err == nil {
-			// If we aren't the first proxy retain prior
-			// X-Forwarded-For information as a comma+space
-			// separated list and fold multiple headers into one.
-			prior, ok := request.Header["X-Forwarded-For"]
-			omit := ok && prior == nil // Issue 38079: nil now means don't populate the header
-			if len(prior) > 0 {
-				clientIP = strings.Join(prior, ", ") + ", " + clientIP
+	flushInterval := ptypes.Duration(100 * time.Millisecond)
+	proxy := &httputil.ReverseProxy{
+		Director: func(outReq *http.Request) {
+			u := outReq.URL
+			if outReq.RequestURI != "" {
+				parsedURL, err := url.ParseRequestURI(outReq.RequestURI)
+				if err == nil {
+					u = parsedURL
+				}
 			}
-			if !omit {
-				req.Header.Set("X-Forwarded-For", clientIP)
+
+			outReq.URL.Path = u.Path
+			outReq.URL.RawPath = u.RawPath
+			outReq.URL.RawQuery = strings.ReplaceAll(u.RawQuery, ";", "&")
+			outReq.RequestURI = "" // Outgoing request should not have RequestURI
+
+			outReq.Proto = "HTTP/1.1"
+			outReq.ProtoMajor = 1
+			outReq.ProtoMinor = 1
+
+			if _, ok := outReq.Header["User-Agent"]; !ok {
+				outReq.Header.Set("User-Agent", "")
 			}
-		}
 
-		err := client.Do(req, res)
-		if err != nil {
-			log.FromContext(request.Context()).Error(err)
-			return
-		}
+			// Even if the websocket RFC says that headers should be case-insensitive,
+			// some servers need Sec-WebSocket-Key, Sec-WebSocket-Extensions, Sec-WebSocket-Accept,
+			// Sec-WebSocket-Protocol and Sec-WebSocket-Version to be case-sensitive.
+			// https://tools.ietf.org/html/rfc6455#page-20
+			outReq.Header["Sec-WebSocket-Key"] = outReq.Header["Sec-Websocket-Key"]
+			outReq.Header["Sec-WebSocket-Extensions"] = outReq.Header["Sec-Websocket-Extensions"]
+			outReq.Header["Sec-WebSocket-Accept"] = outReq.Header["Sec-Websocket-Accept"]
+			outReq.Header["Sec-WebSocket-Protocol"] = outReq.Header["Sec-Websocket-Protocol"]
+			outReq.Header["Sec-WebSocket-Version"] = outReq.Header["Sec-Websocket-Version"]
+			delete(outReq.Header, "Sec-Websocket-Key")
+			delete(outReq.Header, "Sec-Websocket-Extensions")
+			delete(outReq.Header, "Sec-Websocket-Accept")
+			delete(outReq.Header, "Sec-Websocket-Protocol")
+			delete(outReq.Header, "Sec-Websocket-Version")
+		},
+		Transport:     http.DefaultTransport,
+		FlushInterval: time.Duration(flushInterval),
+		// BufferPool:    bufferPool,
+		ErrorHandler: func(w http.ResponseWriter, request *http.Request, err error) {
+			statusCode := http.StatusInternalServerError
 
-		// TODO: Remove Connection Headers
-		for _, header := range hopHeaders {
-			res.Header.Del(header)
-		}
-		res.Header.VisitAll(func(key, value []byte) {
-			writer.Header().Set(string(key), string(value))
-		})
+			switch {
+			case errors.Is(err, io.EOF):
+				statusCode = http.StatusBadGateway
+			case errors.Is(err, context.Canceled):
+				statusCode = StatusClientClosedRequest
+			default:
+				var netErr net.Error
+				if errors.As(err, &netErr) {
+					if netErr.Timeout() {
+						statusCode = http.StatusGatewayTimeout
+					} else {
+						statusCode = http.StatusBadGateway
+					}
+				}
+			}
 
-		writer.WriteHeader(res.StatusCode())
-		res.BodyWriteTo(writer)
-
-		res.Header.VisitAllTrailer(func(key []byte) {
-			writer.Header().Set(string(key), string(res.Header.Peek(string(key))))
-		})
-
-	}), nil
+			log.Debugf("'%d %s' caused by: %v", statusCode, statusText(statusCode), err)
+			w.WriteHeader(statusCode)
+			_, werr := w.Write([]byte(statusText(statusCode)))
+			if werr != nil {
+				log.Debugf("Error while writing status code", werr)
+			}
+		},
+		Client: client,
+	}
+	return proxy, nil
 }
 
 func buildProxy(passHostHeader *bool, responseForwarding *dynamic.ResponseForwarding, roundTripper http.RoundTripper, bufferPool httputil.BufferPool) (http.Handler, error) {
